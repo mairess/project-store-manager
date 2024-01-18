4fab018c4d50cdd6b1db63a75037297d7b282ee6
mairess <trabalho.maires@gmail.com>
2024-01-18 16:31:21 -0300

    remove log

--- backend/src/services/validations/validationsInputValues.js
+++ backend/src/services/validations/validationsInputValues.js
@@ -5,13 +5,10 @@ const validateCreateNewProduct = (keysObjectToValidate) => {
 
   if (error) {
     return { status: error.message.includes('characters')
-      ? 'INVALID_VALUE' : 'BAD_REQUEST',
+      ? 'INVALID_VALUE'
+      : 'BAD_REQUEST',
     message: error.message };
   }
-  // if (error.message.includes('characters')) {
-  //   return { status: 'INVALID_VALUE', message: error.message };
-  // }
-  console.log('error', error);
 };
 
 module.exports = {
