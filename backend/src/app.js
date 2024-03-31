3b1d5d21eeca5e629d1df942c52024e38b88ed5a
mairess <trabalho.maires@gmail.com>
2024-03-31 12:09:09 -0300

    feat: partial, documents products

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const yaml = require('js-yaml');
+const fs = require('fs');
+const cors = require('cors');
 const { productRoutes, saleRoutes } = require('./routes');
+
+const raw = fs.readFileSync('swagger.yaml');
+const data = yaml.load(raw);
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(data));
 
 app.use('/products', productRoutes);
 app.use('/sales', saleRoutes);
